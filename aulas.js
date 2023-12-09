92d20029223b8f644af467d06d6148c23295e61d
Luiz Barbosa <124316232+beardevbits@users.noreply.github.com>
2023-12-09 19:48:44 -0300

    Add files via upload

---
+++ aulas.js
@@ -0,0 +1,230 @@
+let jogadora = prompt(`Seus dois primeiros nomes: `)
+
+alert(`Bem vindo(a), ${jogadora} `)
+
+let work = prompt(`Trabalhou muito hoje? ` )
+
+if (work === `sim`,`Sim`) {
+    alert(`Eu sei meu amor`)
+} else {
+    alert(`Trabalhou sim`)
+}
+
+let work2 = prompt(`Ta cansada?  ` )
+
+
+alert("Quiz valendo um alimento do 99*")
+
+let namorado = prompt(`Qual o primeiro nome do namorado: `)
+
+if (namorado === `Luiz`,`luiz`) {
+    alert(`Acertou`)
+} else {
+    alert(`Faz o bagulho direito`)
+}
+
+let task = prompt(`Qual desses ${namorado} mais gosta: 
+    A-Naruto
+    B-One piece
+    C-Bakugan
+`)
+
+switch (task) {
+    case "a":
+        alert("Acertou")
+        break;
+        case "b":
+            alert("Errou")
+            break
+                 case "c":
+                    alert("Errou")
+                     break;
+                default:
+                    alert(`Brinca na moral`)
+                
+}
+
+
+let task2 = prompt(`Qual desses ${namorado} mais gosta: 
+    A-Coca-cola
+    B-Pepsi
+    C-Limonada
+`)
+
+switch (task2) {
+    case "a":
+        alert("Errou")
+        break;
+        case "b":
+            alert("Errou")
+            break
+                 case "c":
+                    alert("Acertou")
+                     break;
+                default:
+                    alert(`Brinca na moral`)
+                
+}
+
+alert(`Baseado em suas experiencias sexuais...`)
+
+
+
+let task3 = prompt(`Em qual desses pontos o seu namorado é bom?  
+    A-Oral
+    B-Penetração
+    C-TODOS
+`)
+
+switch (task3) {
+    case "a":
+        alert("Errou")
+        break;
+        case "b":
+            alert("Errou")
+            break
+                 case "c":
+                    alert("Acertou")
+                     break;
+                 case "C":
+                    alert("Acertou")
+                     break;
+                default:
+                    alert(`EM TODOS!!!!!!!!`)
+                
+}
+
+
+
+
+let task4 = prompt(`${namorado} prefere?  
+    A-BK
+    B- MC
+    C-Japa
+`)
+
+
+switch (task4) {
+    case "a":
+        alert("Errou")
+        break;
+        case "b":
+            alert("Acertou")
+            break
+                 case "c":
+                    alert("Errou")
+                     break;
+                default:
+                    alert(`Brinca na moral`)
+                
+}
+
+
+let task5 = prompt(`${namorado} tem quantos cm de rola?  
+    A-17
+    B-18
+    C-19
+`)
+
+
+switch (task5) {
+    case "a":
+        alert("Errou")
+        break;
+        case "b":
+            alert("Acertou")
+            break
+                 case "c":
+                    alert("Errou")
+                     break;
+                default:
+                    alert(`Brinca na moral`)
+                
+}
+
+let task6 = prompt(`${namorado} prefere?  
+    A- amor e carinho
+    B- amor e jogo
+    C- jogo e jogo 
+`)
+
+
+switch (task6) {
+    case "a":
+        alert("Errou")
+        break;
+            case "b":
+            alert("Acertou")
+                break
+                     case "c":
+                        alert("eu, tambem mas eu tenho namorada")
+                        break;
+                default:
+                    alert(`Brinca na moral`)
+                
+}
+
+let task7 = prompt(`${namorado} nasceu em?  
+    A- 11/08/02002
+    B- 11/08/2002
+    C- 11/80/2002
+`)
+
+
+switch (task7) {
+    case "a":
+        alert("Errou")
+        break;
+        case "b":
+            alert("Acertou")
+            break
+                 case "c":
+                    alert("errou")
+                     break;
+                default:
+                    alert(`Brinca na moral`)
+                
+}
+
+
+let task8 = prompt(`${namorado} prefere comer:  
+    A-Doritos
+    B-Mega
+    C-Duda
+    D-Japa
+`)
+
+
+switch (task8) {
+    case "a":
+        alert("Errou")
+        break;
+        case "b":
+            alert("errou")
+            break
+                 case "c":
+                    alert("errou")
+                     break;
+                     case "d":
+                    alert("acertou")
+                     break;
+                default:
+                    alert(`Brinca na moral`)
+                
+}
+
+alert(`ULTIMA...`)
+
+let amor = prompt(`Quem ${namorado} mais ama? `)
+
+alert(`${jogadora} EU TE AMO.`)
+
+
+
+
+
+
+
+
+
+
